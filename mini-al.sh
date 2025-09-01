16e320466e88b6c8897af115a214faa9b6dae4ed
Paul Garuba <51539331+goshp@users.noreply.github.com>
2025-09-01 09:51:54 -0400

    Update mini-al.sh

--- mini-al.sh
+++ mini-al.sh
@@ -1,16 +1,42 @@
+kubeMini Installation:
+
 #vim .bashrc
 #export PATH=$PATH:/usr/local/bin
 #source .bashrc
 
-yum install docker -y
-systemctl start docker
-systemctl status docker
+#Set up the Docker repository:
+
+sudo apt update
+sudo apt install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+#Add the Docker repo:
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#Install Docker Engine:
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+Start and enable Docker:
+sudo systemctl enable --now docker
+
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo mv kubectl /usr/local/bin/kubectl
 sudo chmod +x /usr/local/bin/kubectl
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
-sudo yum install iptables -y
-yum install conntrack -y
+
+# Add your user to the Docker group
+sudo usermod -aG docker $USER
+newgrp docker
+
+sudo apt install iptables -y
+sudo apt install conntrack -y
 minikube start --driver=docker --force
 minikube status 
